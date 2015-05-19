ae4806232dc0c67ac68f96c0db5b88aa9dd24062
Mikolaj Piosik <amuso2@gmail.com>
2015-05-19 18:48:27 +0200

    rozgrzebana funkcja

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,10 @@
 def dodawanie():
-    print("Podaj pierwszą liczbę")
-    a = int(input())
-    print("Podaj drugą liczbę")
-    b = int(input())
-    print(a+b)
+    return(a+b)
+
+print("Podaj pierwszą liczbę")
+a = int(input())
+print("Podaj drugą liczbę")
+b = int(input())
+    
 
 dodawanie()
